0159b0029d18d4b4c9a3aa9be2927001bea7f12f
MasterFlame107 <106663437+MasterFlame107@users.noreply.github.com>
2024-12-15 11:20:53 +1100

    adding notes one more time as comments

--- webtry/Website/views.py
+++ webtry/Website/views.py
@@ -23,19 +23,18 @@ def home():
             flash('Note added!', category='success')
 
     return render_template("home.html", user = current_user)
-'''
+
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
+        db.session.commit()
+            
 
     return jsonify({})
-'''
+
 @views.route('/movies', methods=['GET','POST'])
 @login_required
 def Moviepage():
