7b249ca50c7c1b375165af0d5b8ce1b5007f507f
MasterFlame107 <106663437+MasterFlame107@users.noreply.github.com>
2024-12-15 15:04:05 +1100

    fixed bool field

--- webtry/Website/models.py
+++ webtry/Website/models.py
@@ -17,5 +17,5 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    admin_auth = db.Column(db.BooleanField(False))
+    admin_auth = db.Column(db.Boolean)
     notes = db.relationship('Note')
